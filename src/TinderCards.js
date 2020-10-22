268a7a4d32877023603df86c1305b637a424f697
Christian Lewis <christianlewis024@gmail.com>
2020-10-22 10:23:33 -0700

    cards roughly styled and setup to display stacked

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -9,6 +9,18 @@ function TinderCards() {
             url: 'https://images.axios.com/TrgcfEDxI_FLZtY9SmpmC66tJDI=/0x59:3600x2084/1920x1080/2020/10/20/1603199382777.jpg'
         },
         {
+            name: 'mark zuckerberg',
+            url: 'https://images.axios.com/TrgcfEDxI_FLZtY9SmpmC66tJDI=/0x59:3600x2084/1920x1080/2020/10/20/1603199382777.jpg'
+        },
+        {
+            name: 'mark zuckerberg',
+            url: 'https://images.axios.com/TrgcfEDxI_FLZtY9SmpmC66tJDI=/0x59:3600x2084/1920x1080/2020/10/20/1603199382777.jpg'
+        },
+        {
+            name: 'mark zuckerberg',
+            url: 'https://images.axios.com/TrgcfEDxI_FLZtY9SmpmC66tJDI=/0x59:3600x2084/1920x1080/2020/10/20/1603199382777.jpg'
+        },
+        {
             name: 'Mr Morton',
             url: 'https://s3.amazonaws.com/dynamic.wizer.me/uploads%2F264703357-1547205221527-Mr.+Morton+was+very+shy.png'
         }
@@ -17,7 +29,9 @@ function TinderCards() {
     return (
         <div className="tinderCards">
             <h3>I am the tinder cards</h3>
-            {people.map(person => (
+            <div className="tinderCards__cardContainer">
+
+                 {people.map(person => (
                 <TinderCard
                 className="swipe" 
                 key={person.name}
@@ -28,6 +42,8 @@ function TinderCards() {
                     </div>
                 </TinderCard>
             ))}
+            </div>
+           
         </div>
     )
 }
