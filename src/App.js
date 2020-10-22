1ea086d29a8ebcab88627ecdd840e5c99275ab4b
Christian Lewis <christianlewis024@gmail.com>
2020-10-22 10:12:57 -0700

    tinder cards displaying unstyled

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from "./Header"
+import TinderCards from "./TinderCards"
 
 function App() {
   return (
     <div className="app">
-     
+     <Router>
      <Header/>
+       <Switch>
+         <Route path="/chat">
+           <h1> i am the chat page</h1>
+         </Route>
+         <Route path="/">
+          <TinderCards/>
+         </Route>
+     
     
-      {/* tinder cards */}
-      {/* buttons below tinder cards */}
-    {/* chat screen */}
-    {/* individual chat screen */}
-
+     </Switch>
+    </Router>
     </div>
   );
 }
 
 export default App;
+ {/* tinder cards */}
+      {/* buttons below tinder cards */}
+    {/* chat screen */}
+    {/* individual chat screen */}
