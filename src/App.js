d03824f84bc9f44e26735a58a67f7c7ce94a1822
Christian Lewis <christianlewis024@gmail.com>
2020-10-22 09:07:05 -0700

    header component created and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './App.css';
+import Header from "./Header"
 
 function App() {
   return (
     <div className="app">
-     <h1>Hi friends</h1>
-     {/* header */}
+     
+     <Header/>
+    
       {/* tinder cards */}
       {/* buttons below tinder cards */}
     {/* chat screen */}
